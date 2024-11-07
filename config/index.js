20000484656e120ba68e2d8ede560251b7d3b376
qingqingsu <73687188+qingqingsu@users.noreply.github.com>
2024-11-07 12:07:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxc65436b577733387",
+  appsecret: "1c3256366f0d06b720b2c27e012ab4fa",
+  template_id: "3vJQabMUAQLOV1z48AmxNGBE93FKME0oqAooKIt1qdU",
+  touser: "oQe0h6-Ktg2oIvmTbtSS4NqbC4Ko",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "上海",
+  first_date: "2023-7-6",
 };
